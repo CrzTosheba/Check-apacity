93a7104be7bbd1c85590df5c259e975cd3368080
CrzTosheba <fugu@liist.ru>
2022-04-29 10:54:04 +0300

    Add project files.

---
+++ Check2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace PhoneBook
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            System.IO.StreamReader file = new StreamReader(@"C:\CFiles\Text2.txt");
+            String line;
+            var words = new LinkedList<string>();
+
+            // Запустим таймер
+            var watchTwo = Stopwatch.StartNew();
+            
+
+            while ((line = file.ReadLine()) != null)
+            {
+                words.AddLast(line); // вставка в конец примерно такая же как и в простом листе
+               // words.AddFirst(line); // тут время значительно больше
+            }
+
+            watchTwo.Stop();
+
+
+
+            // Выведем результат
+            //foreach (var word in words) //  просто проверял что все что нужно переносится
+            //     Console.WriteLine(word);
+            Console.WriteLine($"Вставка в  словарь: {watchTwo.Elapsed.TotalMilliseconds}  мс");
+            file.Close();
+        }
+    }
+
+}
---
+++ CheckСapacity/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace PhoneBook
+{
+    class Program
+    {
+              
+        static void Main(string[] args)
+        {
+            System.IO.StreamReader file = new StreamReader(@"C:\CFiles\Text2.txt");
+            String line;
+            var words = new List<string>();
+            // Запустим таймер
+            var watchTwo = Stopwatch.StartNew();
+
+            while ((line = file.ReadLine()) != null)
+            {
+                words.Add(line);
+                
+            }
+
+            watchTwo.Stop();
+
+
+
+
+            // Выведем результат
+
+            //foreach (var word in words)
+            //     Console.WriteLine(word);
+            Console.WriteLine($"Вставка в  словарь: {watchTwo.Elapsed.TotalMilliseconds}  мс");
+            file.Close();
+        }
+    }
+   
+}
